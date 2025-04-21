ba2dc8b1d1fdbcf03df486d7c3327c44a0a10c75
DicoJhor <jhordan.vt11@gmail.com>
2025-04-20 21:40:11 -0500

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,13 @@
+// Lista de productos de ejemplo
+const productos = [
+    { id: 1, nombre: "Hamburguesa", precio: 15.50 },
+    { id: 2, nombre: "Pizza", precio: 20.00 },
+    { id: 3, nombre: "Ensalada César", precio: 12.00 },
+    { id: 4, nombre: "Sopa de Pollo", precio: 10.00 },
+    { id: 5, nombre: "Tacos", precio: 18.00 },
+    { id: 6, nombre: "Papas Fritas", precio: 8.00 },
+    { id: 7, nombre: "Cóctel de Frutas", precio: 9.00 },
+    { id: 8, nombre: "Refresco", precio: 5.00 },
+    { id: 9, nombre: "Cerveza", precio: 10.00 }
+  ];
+  
